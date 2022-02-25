23094360675c049f2a92fbda5a7314c496dfa4f8
Austin P <austin.paquette@forcir.com>
2022-02-24 23:20:52 -0800

    update next.js config (#3)

--- next.js
+++ next.js
@@ -1,29 +1,4 @@
 module.exports = {
-  extends: ['./frontend.js'],
-  plugins: ['node', 'react', 'react-hooks'],
-  env: {
-    browser: true,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  overrides: [
-    {
-      files: ['pages/**/*.tsx'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-      },
-    },
-  ],
-  rules: {
-    'node/no-unpublished-require': 'off',
-
-    'react/prop-types': 'off',
-
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
+  extends: ['./index.js'],
+  plugins: ['next'],
 };
