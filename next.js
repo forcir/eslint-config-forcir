572d8c0af3a18d97adb6adec5a3597bb98530141
Austin Paquette <austin.paquette@forcir.com>
2022-02-24 23:14:48 -0800

    update next.js config

--- next.js
+++ next.js
@@ -1,30 +1,4 @@
 module.exports = {
   extends: ['./index.js'],
   plugins: ['next'],
-  env: {
-    browser: true,
-    node: true,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  overrides: [
-    {
-      files: ['pages/**/*.tsx'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-      },
-    },
-  ],
-  rules: {
-    'node/no-unpublished-require': 'off',
-
-    'react/prop-types': 'off',
-
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
 };
