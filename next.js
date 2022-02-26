689776fdc6ab0a337a4901e931ec564d4688eea7
Austin Paquette <austin.paquette@forcir.com>
2022-02-25 18:59:24 -0800

    add overrides for next pages

--- next.js
+++ next.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['./index.js', 'next'],
+  overrides: [
+    {
+      files: ['pages/**/*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
 };
